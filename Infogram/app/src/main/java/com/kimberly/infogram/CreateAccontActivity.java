6a60638b8108157ba6627ae9ee658841f2a8635e
KimberlyPeredo <kimberlyperedo575@gmail.com>
2025-09-01 12:07:53 -0400

    infogram

--- Infogram/app/src/main/java/com/kimberly/infogram/CreateAccontActivity.java
+++ Infogram/app/src/main/java/com/kimberly/infogram/CreateAccontActivity.java
@@ -1,29 +1,34 @@
 package com.kimberly.infogram;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class CreateAccontActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_create_accont);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        showToolBar("Crear cuenta",true);
     }
-    public void goCreateAcoount(View view) {
-        Intent intent = new Intent(this, CreateAccontActivity.class);
-        startActivity(intent);
+    public void showToolBar(String titulo, boolean botonSubir) {
+        Toolbar toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
     }
-}
+    }
